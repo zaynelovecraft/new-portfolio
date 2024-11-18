dd4be4ae5a4c8c69c8b86366e4082812aa9732ef
EpicDevv <zayne.mngmt@gmail.com>
2024-11-17 21:41:35 -0700

    fix: update info

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -40,20 +40,6 @@ const Resume = ({ data }) => {
 
   return (
     <section id="resume">
-      <div className="row education">
-        <div className="three columns header-col">
-          <h1>
-            <span>Education</span>
-          </h1>
-        </div>
-
-        <div className="nine columns main-col">
-          <div className="row item">
-            <div className="twelve columns">{education}</div>
-          </div>
-        </div>
-      </div>
-
       <div className="row work">
         <div className="three columns header-col">
           <h1>
@@ -76,6 +62,20 @@ const Resume = ({ data }) => {
 
           <div className="bars">
             <ul className="skills">{skills}</ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">{education}</div>
           </div>
         </div>
       </div>
